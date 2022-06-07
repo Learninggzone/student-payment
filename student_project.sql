11a56ba93d043df008ed88d2a36615986fd27dd8
shyngysnurzhan <nurzhanshyngys@gmail.com>
2022-06-08 01:25:40 +0600

    выпуск 36 - таблицы student_order, child

--- student_project.sql
+++ student_project.sql
@@ -1,33 +1,98 @@
-drop table if exists sp_street;
-drop table if exists sp_country_struct;
-drop table if exists sp_passport_office;
-drop table if exists sp_register_office;
+DROP TABLE IF EXISTS jc_student_child;
+DROP TABLE IF EXISTS jc_student_order;
+DROP TABLE IF EXISTS jc_passport_office;
+DROP TABLE IF EXISTS jc_register_office;
+DROP TABLE IF EXISTS jc_country_struct;
+DROP TABLE IF EXISTS jc_street;
 
-create table sp_street(
+CREATE TABLE jc_street
+(
     street_code integer not null,
     street_name varchar(300),
-    primary key (street_code)
+    PRIMARY KEY (street_code)
 );
 
-create table sp_country_struct
+CREATE TABLE jc_country_struct
 (
     area_id char(12) not null,
     area_name varchar(200),
-    primary key (area_id)
+    PRIMARY KEY (area_id)
 );
 
-create table sp_passport_office(
+CREATE TABLE jc_passport_office
+(
     p_office_id integer not null,
     p_office_area_id char(12) not null,
     p_office_name varchar(200),
-    primary key(p_office_id),
-    foreign key(p_office_area_id) references sp_country_struct(area_id) on delete restrict
+    PRIMARY KEY (p_office_id),
+    FOREIGN KEY (p_office_area_id) REFERENCES jc_country_struct(area_id) ON DELETE RESTRICT
 );
 
-create table sp_register_office(
+CREATE TABLE jc_register_office
+(
     r_office_id integer not null,
     r_office_area_id char(12) not null,
     r_office_name varchar(200),
-    primary key(r_office_id),
-    foreign key(r_office_area_id) references sp_country_struct(area_id) on delete restrict
+    PRIMARY KEY (r_office_id),
+    FOREIGN KEY (r_office_area_id) REFERENCES jc_country_struct(area_id) ON DELETE RESTRICT
+);
+
+
+CREATE TABLE jc_student_order
+(
+    student_order_id SERIAL,
+    h_sur_name varchar(100) not null,
+    h_given_name varchar(100) not null,
+    h_patronymic varchar(100) not null,
+    h_date_of_birth date not null,
+    h_passport_seria varchar(10) not null,
+    h_passport_number varchar(10) not null,
+    h_passport_date date not null,
+    h_passport_office_id integer not null,
+    h_post_index varchar(10),
+    h_street_code integer not null,
+    h_building varchar(10) not null,
+    h_extension varchar(10),
+    h_apartment varchar(10),
+    w_sur_name varchar(100) not null,
+    w_given_name varchar(100) not null,
+    w_patronymic varchar(100) not null,
+    w_date_of_birth date not null,
+    w_passport_seria varchar(10) not null,
+    w_passport_number varchar(10) not null,
+    w_passport_date date not null,
+    w_passport_office_id integer not null,
+    w_post_index varchar(10),
+    w_street_code integer not null,
+    w_building varchar(10) not null,
+    w_extension varchar(10),
+    w_apartment varchar(10),
+    certificate_id varchar(20) not null,
+    register_office_id integer not null,
+    marriage_date date not null,
+    PRIMARY KEY (student_order_id),
+    FOREIGN KEY (h_street_code) REFERENCES jc_street(street_code) ON DELETE RESTRICT,
+    FOREIGN KEY (w_street_code) REFERENCES jc_street(street_code) ON DELETE RESTRICT,
+    FOREIGN KEY (register_office_id) REFERENCES jc_register_office(r_office_id) ON DELETE RESTRICT
+);
+
+CREATE TABLE jc_student_child
+(
+    student_child_id SERIAL,
+    student_order_id integer not null,
+    c_sur_name varchar(100) not null,
+    c_given_name varchar(100) not null,
+    c_patronymic varchar(100) not null,
+    c_date_of_birth date not null,
+    c_certificate_number varchar(10) not null,
+    c_certificate_date date not null,
+    c_register_office_id integer not null,
+    c_post_index varchar(10),
+    c_street_code integer not null,
+    c_building varchar(10) not null,
+    c_extension varchar(10),
+    c_apartment varchar(10),
+    PRIMARY KEY (student_child_id),
+    FOREIGN KEY (c_street_code) REFERENCES jc_street(street_code) ON DELETE RESTRICT,
+    FOREIGN KEY (c_register_office_id) REFERENCES jc_register_office(r_office_id) ON DELETE RESTRICT
 );
